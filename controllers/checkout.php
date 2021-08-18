b160f66ae2f7350b660cf49b0aa08cc8cd91f003
Timothy Ehimen <tehi0001@student.monash.edu>
2021-08-18 19:35:16 +0200

    implemented authenticated user checkout

--- controllers/checkout.php
+++ controllers/checkout.php
@@ -12,7 +12,11 @@ $shipping = $post['order']['shipping'];
 
 try {
     if(isset($headers['Authorization'])) {
-        $user = Utils::get_user_from_session($response);
+        $user_email = Utils::get_user_from_session($response);
+
+        $user_info = $db->select_one("SELECT id FROM users WHERE email=? AND is_registered=?", "si", array($user_email, 1));
+
+        $user = $user_info['id'];
     }
     else {
 
